b391589e4270e387f2129062a7bf0304e8b66ced
PanchoDM <carfranchescodm0207@gmail.com>
2026-05-31 21:44:27 -0500

    adaptar commit para supab

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,14 @@
 const { Pool } = require('pg');
 
-// Conectamos escribiendo los datos directamente, saltándonos Render
 const pool = new Pool({
-  host: 'aws-0-us-east-1.pooler.supabase.com',
-  port: 6543,
-  user: 'postgres.flvcxotyxesyvqbjqvxm',
-  password: 'TalmaBase2026', // <-- BORRA ESTO Y PON TU CONTRASEÑA REAL AQUÍ
-  database: 'postgres',
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
 });
 
-// Esta función hará una prueba de conexión apenas el servidor encienda
-pool.connect((err, client, release) => {
-  if (err) {
-    console.error('❌ ERROR FATAL AL CONECTAR A LA BASE DE DATOS:', err);
-  } else {
-    console.log('✅ ¡CONEXIÓN A SUPABASE EXITOSA!');
-    release();
-  }
+pool.on('error', (err) => {
+  console.error('Error inesperado en la base de datos:', err);
 });
 
 module.exports = pool;
